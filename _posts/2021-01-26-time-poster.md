74eebb0dc2a69764f8ea531bd97d86ffdabccf49
Poire Molle <lapoiremolle@gmail.com>
2021-01-28 23:15:54 +0300

    upload time poster post

---
+++ _posts/2021-01-26-time-poster.md
@@ -0,0 +1,9 @@
+---
+layout: post
+title: "time poster"
+categories: project
+banner: /images/timeposter.png
+sketch: timeposter.js
+---
+
+First post of 2021 ! I'm interested as always into art not made by people who call themselves artists, the so-called "non-art". Sometimes it takes the form of used up Pictionary drawing pads, or a phrase left on a whiteboard from a previous class. Sometimes the "non-artist" is a computer, simply executing a code. This is what this is. The shapes are placed on the screen according to the time found on your computer. 
---
+++ p5-sketches/timeposter.js
@@ -0,0 +1,56 @@
+
+function setup() {
+    canvas = createCanvas(740, 556);
+        canvas.parent('canvasp');
+    
+    
+    
+    angleMode(DEGREES);
+    
+    rectMode(CENTER);
+}
+
+function draw() {
+    
+background(255, 240, 230);
+let h = hour();
+let m = minute();
+let colFirk = 5;
+let rowFirk = 7;
+    
+    for (let p = 0; p < colFirk; p++) {
+        for(let b = 0; b < rowFirk; b++) {
+            push();
+            translate(width / (colFirk - 1) * p, height / (rowFirk - 1) * b);
+                rotate(m * (p + b));
+                noStroke();
+            fill(255);
+                rect(45, 45, 30, 30);
+            pop();
+        }
+    }
+
+ 
+let colDots = 15;
+let rowDots = 20;
+    for (let i = 0; i < colDots; i++) {
+        for (let j = 0; j < rowDots; j++) {
+        push();
+        translate((width / colDots - 1) * i, (height / rowDots - 1) * j);
+            rotate(h * (i + j));
+            strokeWeight(9);
+            stroke(0, 0, 255);
+            point(45 * i, 41 * j);
+        pop();
+        }
+        
+    }
+    fill(0, 0, 255);
+    textSize(42);
+    textFont('Courier New');
+    if (m > 9){ 
+        text(h + ':' + m, 25, height - 25);
+    }else {text(h + ': 0' + m, 25, height - 25);}
+
+    
+}
