5600607d0352df56fa9a30d3fc82636a4839e1ff
Poire Molle <lapoiremolle@gmail.com>
2021-02-07 23:25:48 +0100

    updating first timeposter cover img and adding second time poster

--- p5-sketches/timeposter2.js
+++ p5-sketches/timeposter2.js
@@ -13,58 +13,45 @@ function setup() {
 
 function draw() {
     
-background(255, 240, 230);
+
 let h = hour();
 let m = minute();
+let s = second();
 let colFirk = 7;
-let rowFirk = 7;
-    size = 30;
+let rowFirk = 10;
+    size = 30 + h;
 
+ background(255 - (h * 10), (100 + m) - (h * 7), 230 - (h * 9));
     
     for (let p = 0; p < colFirk; p++) {
         for(let b = 0; b < rowFirk; b++) {
 
             push();
             translate(width / (colFirk - 1) * p + m, height / (rowFirk - 1) * b + h);
-                rotate(m * (p * b));
+                rotate(10 + m * (p * b));
                 noStroke();
-            fill(255,200,240,200);
-                rect(5 * p + h, 5 * b + m, size * p, size);
-            
-           rotate(3);
-            fill(255);
-            rect(p * 5, b * 40, 5, 50 - (p * 5));
+            fill(155-h,195+m,175+s,205);
+                rect(5 * p + h, 5 * b + m, size * p, size / b);
             pop();
         }
     }
 
  
-let colDots = 15;
+let colDots = 35;
 let rowDots = 20;
     for (let i = 0; i < colDots; i++) {
         for (let j = 0; j < rowDots; j++) {
         push();
         translate((width / colDots - 1) * i, (height / rowDots - 1) * j);
             rotate(h * (i + j));
-           
-            
-            //BLUE DOTS
-            strokeWeight(9);
-            stroke(0, 0, 255);
+            strokeWeight(9 + i);
+            stroke(h * 10, h * 10, 255 - (h * 10));
             point(45 * i, 41 * j);
-            
-            
-            
-            //WHITE DOTS
-//            strokeWeight(3);
-//            stroke(255);
-//            point(45 * i + (i*j), 41 * i);
-//            point(45 * i + 45, 41 * j + 45);
         pop();
         }
         
     }
-    fill(0, 0, 255);
+    fill(h * 10, h * 10, 255 - (h * 10));
     textSize(42);
     textFont('Courier New');
     if (m > 9){ 
